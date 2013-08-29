aeda522eb5be81f51c320c9f1e3283ea2bbdd1b8
Novatux <nathanael.courant@laposte.net>
2013-08-29 09:34:40 +0200

    Make power distribution more efficient.

--- technic/machines/register/cables.lua
+++ technic/machines/register/cables.lua
@@ -59,6 +59,8 @@ minetest.register_on_placenode(function(pos, node)
 		for machine_name, _ in pairs(machine_list) do
 			if node.name == machine_name then
 				technic.update_cables(pos, node.name, tier, true)
+				technic.networks = {}
+				return
 			end
 		end
 	end
@@ -70,6 +72,8 @@ minetest.register_on_dignode(function(pos, node)
 		for machine_name, _ in pairs(machine_list) do
 			if node.name == machine_name then
 				technic.update_cables(pos, node.name, tier, true)
+				technic.networks = {}
+				return
 			end
 		end
 	end
